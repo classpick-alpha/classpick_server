aba6c657ae837fafc62b70ea94dbd2a4d4c6bd9c
Son Daehyeon <sondaehyeon01@gmail.com>
2025-05-05 16:00:43 +0900

    chore(default-file): swagger 문서 렌더링 제외

--- src/main/java/com/github/classpick/default_file/controller/DefaultFileController.java
+++ src/main/java/com/github/classpick/default_file/controller/DefaultFileController.java
@@ -6,6 +6,7 @@ import com.github.classpick.default_file.controller.dto.SaveDefaultFileRes;
 import com.github.classpick.default_file.repository.DefaultFileEntity;
 import com.github.classpick.default_file.service.DefaultFileService;
 import com.github.classpick.global.dto.Response;
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+@Hidden
 @RequiredArgsConstructor
 @RestController
 public class DefaultFileController {
@@ -26,26 +28,24 @@ public class DefaultFileController {
     @PostMapping("/v0.0/defaultfile")
     public Response<SaveDefaultFileRes> saveDefaultFile(
             @RequestParam("file") MultipartFile file,
-            // TODO: userId를 직접 받는 것은 보안 상 위험함으로,
-            //  추후 인증 구현 후 JWT 토큰에서 사용자 정보 추출하는 방식으로 수정
-            @RequestParam("userId") Long userId) {
+            @RequestParam("userId") Long userId
+    ) {
 
         defaultFileService.saveDefaultFile(file, userId);
         return Response.ok();
     }
 
-    // TODO: userId를 직접 받는 것은 보안 상 위험함으로,
-    //  추후 인증 구현 후 JWT 토큰에서 사용자 정보 추출하는 방식으로 수정
     @GetMapping("/v0.0/file/{userID}")
     public Response<List<GetDefaultFileInfoRes>> getDefaultFile(@PathVariable Long userID) {
+
         List<DefaultFileEntity> fileList = defaultFileService.getDefaultFile(userID);
         return Response.ok(GetDefaultFileInfoRes.of(fileList));
     }
 
     @DeleteMapping("/v0.0/file/{fileId}")
     public Response<Void> deleteDefaultFile(@PathVariable Long fileId) {
+
         defaultFileService.deleteDefaultFile(fileId);
         return Response.ok();
     }
-
 }
