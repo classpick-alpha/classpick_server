1f60dbc447f0346bc9e687b4848b3b59d3c95842
wngktjd13 <seonguk3553@kookmin.ac.kr>
2025-05-13 21:39:34 +0900

    [CP-217] refactor: S3 key 생성을 UploadService에서 분리하여 S3KeyFactory로 이전

--- src/main/java/com/github/classpick/reservation/service/ReservationService.java
+++ src/main/java/com/github/classpick/reservation/service/ReservationService.java
@@ -1,14 +1,14 @@
 package com.github.classpick.reservation.service;
 
 import com.github.classpick.file.upload.dto.UploadImageResponse;
-import com.github.classpick.file.upload.service.UploadService;
+import com.github.classpick.file.upload.util.S3KeyFactory;
+import com.github.classpick.global.external.aws.s3.S3Service;
 import com.github.classpick.global.user.UserGetter;
 import com.github.classpick.reservation.controller.dto.request.CreateReservationRequest;
 import com.github.classpick.reservation.controller.dto.response.ReservationListResponse;
 import com.github.classpick.reservation.controller.dto.response.ReservationResponse;
 import com.github.classpick.reservation.exception.ReservationException;
 import com.github.classpick.reservation.exception.ReservationExceptionCode;
-import com.github.classpick.reservation.repository.ProofType;
 import com.github.classpick.reservation.repository.ReservationEntity;
 import com.github.classpick.reservation.repository.ReservationRepository;
 import com.github.classpick.reservation.repository.Status;
@@ -30,7 +30,7 @@ public class ReservationService {
     private final RoomRepository roomRepository;
 
     private final UserGetter userGetter;
-    private final UploadService uploadService;
+    private final S3Service s3Service;
 
     @Transactional
     public ReservationResponse createReservation(long roomId, CreateReservationRequest dto) {
@@ -45,17 +45,9 @@ public class ReservationService {
             throw new ReservationException(ReservationExceptionCode.RESERVATION_ALREADY_EXIST);
         }
 
-        ReservationEntity reservation = ReservationEntity.builder()
-                .user(user)
-                .room(room)
-                .purpose(dto.getPurpose())
-                .people(dto.getPeople())
-                .date(dto.getDate())
-                .startTime(dto.getStartTime())
-                .endTime(dto.getEndTime())
-                .comment(dto.getComment())
-                .status(Status.REQUESTED)
-                .build();
+        ReservationEntity reservation = ReservationEntity.builder().user(user).room(room).purpose(dto.getPurpose())
+                .people(dto.getPeople()).date(dto.getDate()).startTime(dto.getStartTime()).endTime(dto.getEndTime())
+                .comment(dto.getComment()).status(Status.REQUESTED).build();
 
         reservation = reservationRepository.save(reservation);
 
@@ -83,15 +75,32 @@ public class ReservationService {
 
         UserEntity user = userGetter.getUser();
 
-        List<ReservationResponse> reservations = reservationRepository.findByUser_UserId(user.getUserId())
-                .stream()
-                .map(ReservationResponse::from)
-                .toList();
+        List<ReservationResponse> reservations = reservationRepository.findByUser_UserId(user.getUserId()).stream()
+                .map(ReservationResponse::from).toList();
 
         return ReservationListResponse.of(reservations);
     }
 
-    public UploadImageResponse generateProofUploadUrl(Long reservationId, ProofType type) {
-        return uploadService.uploadImage("reservations", reservationId, "proofs/" + type.toPathname());
+    public UploadImageResponse generateOcrImage(Long reservationId) {
+        reservationRepository.findById(reservationId).stream()
+                .peek((reservation) -> {
+                    if (!reservation.getUser().equals(userGetter.getUser())) {
+                        throw new ReservationException(ReservationExceptionCode.RESERVATION_USER_NOT_MATCH);
+                    }
+                }).findFirst()
+                .orElseThrow(() -> new ReservationException(ReservationExceptionCode.RESERVATION_NOT_FOUND));
+
+        return UploadImageResponse.of(s3Service.generatePresignedUrl(S3KeyFactory.reservatioOcrKey(reservationId)));
+    }
+
+    public UploadImageResponse generateCleanUpImage(Long reservationId) {
+        reservationRepository.findById(reservationId).stream()
+                .peek((reservation) -> {
+                    if (!reservation.getUser().equals(userGetter.getUser())) {
+                        throw new ReservationException(ReservationExceptionCode.RESERVATION_USER_NOT_MATCH);
+                    }
+                }).findFirst()
+                .orElseThrow(()-> new ReservationException(ReservationExceptionCode.RESERVATION_NOT_FOUND));
+        return UploadImageResponse.of(s3Service.generatePresignedUrl(S3KeyFactory.reservatioCleanUpKey(reservationId)));
     }
 }
